2836aed2a7ffc50c62f9a36c9274f1859863d9c9
saahilster <saahilfardhook@gmail.com>
2026-01-02 19:50:33 -0800

    upd

--- RiffGame/Assets/Scripts/PartySystem/AddMember.cs
+++ RiffGame/Assets/Scripts/PartySystem/AddMember.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
 using System.IO.Ports;
+using System.Globalization;
+using UnityEngine.TextCore.Text;
+using UnityEditor.Experimental.GraphView;
 
 //Script meant to send a summon call to Nano then register the data.
 public class AddMember : MonoBehaviour
@@ -7,7 +10,7 @@ public class AddMember : MonoBehaviour
     SerialPort port = new SerialPort("COM4", 9600);
     string summon = "SUMMON";
     string data;
-    [SerializeField] CharacterDataBase dataBase;
+    [SerializeField] CharacterDataBase figureBase;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -43,6 +46,16 @@ public class AddMember : MonoBehaviour
         }
         data = port.ReadLine();
         Debug.Log($"Nano sent: {data}");   
-        int recievedID = int.Parse(data.Substring(0,2));
+        int recievedID = int.Parse(data.Substring(0,2), NumberStyles.HexNumber);
+
+        for (int i = 0; i > figureBase.database.Count - 1; i++)
+        {
+            if (figureBase.database[i].CharacterID == recievedID)
+            {
+                Debug.Log("Found character!");
+                break;
+            }
+            else{ Debug.Log("Not found yet");}
+        }
     }
 }
